8ae846757c6138a49020f93b47277791b8b0c773
AD2580 <andresdiaz.7102@gmail.com>
2022-04-04 02:11:44 -0300

    Se creó el proyecto

---
+++ src/TestPackage/Main_class.java
@@ -0,0 +1,19 @@
+//Create the project
+
+package TestPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Main_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Andrés Diaz\\eclipse-workspace\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com.ar");
+		driver.manage().window().maximize();
+		
+	}
+
+}
