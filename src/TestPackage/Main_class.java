abb9c4b89c399431805b47fdcaa1bad924d4d108
AD2580 <andresdiaz.7102@gmail.com>
2022-04-04 02:36:16 -0300

    Maximize la ventana y la configuré para que se cierre en 7 segs

--- src/TestPackage/Main_class.java
+++ src/TestPackage/Main_class.java
@@ -8,10 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Andrés Diaz\\eclipse-workspace\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com.ar");
+		driver.manage().window().maximize();
 		
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
 	}
-
 }
